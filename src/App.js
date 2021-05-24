e160fe4109cbd019f16c13eb8503c57691b7dda0
lims4010 <lims4010@gmail.com>
2021-05-23 23:16:46 -0400

    create react app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+// import "./App.css";
+import "antd/dist/antd.css";
+import InputTable from "./components/InputTable";
+
+function App() {
+  return (
+    <div className="App">
+      <InputTable />
+    </div>
+  );
+}
+
+export default App;
